02757d48a897dfb3100b36f02fd142839a6361f9
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-03-21 16:43:56 +0200

    Changes to be committed: 	modified:   src/Screens/CreateScreen.js

--- src/Screens/CreateScreen.js
+++ src/Screens/CreateScreen.js
@@ -1,17 +1,44 @@
-import React from 'react'
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput , Button } from "react-native";
 import { Context } from "../Context/BlogContext";
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text style={styles.label}>Enter Title :</Text>
+      <TextInput
+        style={styles.Input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content :</Text>
+      <TextInput
+        style={styles.Input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
+      <Button title="Add New Blog Post" />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  
-})
+  Input: {
+    borderWidth: 1,
+    borderColor: "black",
+    fontSize: 18,
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft:5
+  },
+});
 
-export default CreateScreen
+export default CreateScreen;
